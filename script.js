f70938af37e2d4aac80c4ed56c7d377cbbab070e
Gustavo Prardo <130465875+DevBaraka@users.noreply.github.com>
2023-08-07 17:30:50 -0300

    4 commit
    correção dos inputs para que possa ser feita a lista antes de por os preços

--- script.js
+++ script.js
@@ -1,36 +1,61 @@
-let botao = document.querySelector('.botao');
-let itemName = document.querySelector('.item-name');
-let itemPreco = document.querySelector('.item-preco');
-let itemList = document.querySelector('.item-List')
 let totalValueInput = document.querySelector(".totalValue");
 let totalValue = 0;
-let myInput = document.querySelector('.botao');
 
-
-window.addEventListener('resize', function() {
-    
-
-    if(window.innerWidth <=380) {
-        myInput.placeholder = 'Adicionar';
-    }
-});
 
 function clicou() {
-    let newItemText = itemName.value;
-    let itemPrecoValue = parseFloat(itemPreco.value);
+    let itemName = document.querySelector('.item-name');
+    let itemList = document.querySelector('.item-List')
 
-    if (newItemText !== '' && !isNaN(itemPrecoValue) && itemPrecoValue > 0) {
+    let newItemText = itemName.value;
+
+    if (newItemText !== '') {
         let newItem = document.createElement('li');
 
-        newItem.textContent = newItemText + ' - Preço: R$:' + itemPrecoValue;
+        let priceInput = document.createElement('input');
+        priceInput.type = 'number';
+        priceInput.className = 'item-price';
+        priceInput.placeholder = 'Preço do item';
+
+        let checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.className = 'item-checkbox';
+
+        newItem.textContent = newItemText + ' - Preço: R$:';
         itemList.appendChild(newItem);
 
-        totalValue += itemPrecoValue;
-        totalValueInput.value = totalValue.toFixed(2);
+        newItem.appendChild(checkbox);
+        itemList.appendChild(newItem);
+        newItem.appendChild(priceInput);
+
+        priceInput.addEventListener('input', function() {
+        calculateTotal();
+        });
+
+        checkbox.addEventListener('change', function() {
+            if (checkbox.checked) {
+            calculateTotal();
+            }
+        });
 
         itemName.value = '';
-        itemPreco.value = '';
     } else {
         window.alert('Você nao preencheu os campos corretamente!')
     }
 }
+
+    function calculateTotal() {
+        totalValue = 0;
+        let priceInputs = document.querySelectorAll('.item-price');
+        let checkboxes = document.querySelectorAll('.item-checkbox');
+    
+        for (let i = 0; i < priceInputs.length; i++) {
+        let itemPriceValue = parseFloat(priceInputs[i].value);
+        if (!isNaN(itemPriceValue)) {
+            if (checkboxes[i].checked) {
+            totalValue += itemPriceValue;
+            }
+        }
+        }
+    
+        totalValueInput.value = totalValue.toFixed(2);
+    }
